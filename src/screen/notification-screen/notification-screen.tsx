842606e02673632d7ce77502678fd413dfa94c05
sardor008 <sardorwebdev@gmail.com>
2025-02-21 22:30:14 +0500

    sef

--- src/screen/notification-screen/notification-screen.tsx
+++ src/screen/notification-screen/notification-screen.tsx
@@ -1,17 +1,183 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export const NotificationScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>NotificationScreen</Text>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          paddingBottom: 20,
+          borderBottomWidth: 2,
+          borderBottomColor: "#E7E9EC",
+        }}
+      >
+        <View style={{ flexDirection: "row", gap: 15 }}>
+          <Ionicons name="notifications" size={24} color="black" />
+          <Text
+            style={{
+              fontFamily: "Overpass",
+              fontWeight: "700",
+              fontSize: 16,
+              lineHeight: 20.26,
+            }}
+          >
+            Notifications
+          </Text>
+        </View>
+        <Text
+          style={{
+            fontFamily: "Overpass",
+            fontWeight: "500",
+            fontSize: 13,
+            lineHeight: 16.46,
+            color: "#1987FB",
+          }}
+        >
+          Clear all
+        </Text>
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          paddingTop: 20,
+          paddingBottom: 20,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            gap: 10,
+            borderBottomWidth: 2,
+            borderBottomColor: "#E7E9EC",
+            paddingBottom: 20,
+          }}
+        >
+          <Ionicons name="calendar-number-sharp" size={24} color="black" />
+          <View style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+            <Text
+              style={{
+                width: 300,
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 14,
+                lineHeight: 17,
+                color: "#091C3F",
+              }}
+            >
+              The future of professional learning is immersive, communal
+              experiences for{" "}
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 13,
+                lineHeight: 16.46,
+                color: "#091C3F73",
+              }}
+            >
+              Aug 12, 2020 at 12:08 PM
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            gap: 10,
+            borderBottomWidth: 2,
+            borderBottomColor: "#E7E9EC",
+            paddingBottom: 20,
+          }}
+        >
+          <Ionicons name="notifications-outline" size={24} color="black" />
+          <View style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+            <Text
+              style={{
+                width: 300,
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 14,
+                lineHeight: 17,
+                color: "#091C3F",
+              }}
+            >
+              The future of professional learning is immersive, communal
+              experiences for{" "}
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 13,
+                lineHeight: 16.46,
+                color: "#091C3F73",
+              }}
+            >
+              Aug 12, 2020 at 12:08 PM
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            gap: 10,
+            borderBottomWidth: 2,
+            borderBottomColor: "#E7E9EC",
+            paddingBottom: 20,
+          }}
+        >
+          <Ionicons
+            name="notifications-circle-outline"
+            size={24}
+            color="black"
+          />
+          <View style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+            <Text
+              style={{
+                width: 300,
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 14,
+                lineHeight: 17,
+                color: "#091C3F",
+              }}
+            >
+              The future of professional learning is immersive, communal
+              experiences for{" "}
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "400",
+                fontSize: 13,
+                lineHeight: 16.46,
+                color: "#091C3F73",
+              }}
+            >
+              Aug 12, 2020 at 12:08 PM
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: 70,
+    marginHorizontal: 20,
   },
 });
--- src/screen/profile-screen/profile-screen.tsx
+++ src/screen/profile-screen/profile-screen.tsx
@@ -1,17 +1,166 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export const ProfileScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>My Profile</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          display: "flex",
+          gap: 10,
+          alignItems: "center",
+          marginTop: 30,
+        }}
+      >
+        <Image
+          source={require("../../../assets/avatar.jpg")}
+          style={{ width: 63, height: 62, borderRadius: 50 }}
+        />
+        <View style={{}}>
+          <Text
+            style={{
+              fontFamily: "Overpass",
+              fontWeight: "400",
+              fontSize: 20,
+              lineHeight: 25.32,
+            }}
+          >
+            Hi, Rahul kanjariya
+          </Text>
+          <Text
+            style={{
+              fontFamily: "Overpass",
+              fontWeight: "400",
+              fontSize: 14,
+              lineHeight: 17.72,
+            }}
+          >
+            Welcome to Nilkanth Medical Store
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          marginTop: 36,
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            borderBottomColor: "#000",
+            borderBottomWidth: 1,
+            paddingBottom: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 20, alignItems: "center" }}>
+            <Ionicons name="list" size={24} color="black" />
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "500",
+                fontSize: 18,
+              }}
+            >
+              Edit Profile
+            </Text>
+          </View>
+          <Ionicons name="chevron-forward" size={18} color="black" />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            borderBottomColor: "#000",
+            borderBottomWidth: 1,
+            paddingBottom: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 20, alignItems: "center" }}>
+            <Ionicons name="options-sharp" size={24} color="black" />
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "500",
+                fontSize: 18,
+              }}
+            >
+              My orders
+            </Text>
+          </View>
+          <Ionicons name="chevron-forward" size={18} color="black" />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            borderBottomColor: "#000",
+            borderBottomWidth: 1,
+            paddingBottom: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 20, alignItems: "center" }}>
+            <Ionicons name="wallet" size={24} color="black" />
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "500",
+                fontSize: 18,
+              }}
+            >
+              Billing
+            </Text>
+          </View>
+          <Ionicons name="chevron-forward" size={18} color="black" />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            borderBottomColor: "#000",
+            borderBottomWidth: 1,
+            paddingBottom: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 20, alignItems: "center" }}>
+            <Ionicons name="help" size={24} color="black" />
+            <Text
+              style={{
+                fontFamily: "Overpass",
+                fontWeight: "500",
+                fontSize: 18,
+              }}
+            >
+              Faq
+            </Text>
+          </View>
+          <Ionicons name="chevron-forward" size={18} color="black" />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: 70,
+    marginHorizontal: 20,
   },
 });
